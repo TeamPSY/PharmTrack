5b0ad5a4fa245f0fb8059afa50054ae731070e3d
bum2067 <bum2067@naver.com>
2025-11-28 17:26:36 +0900

    약품등록 기능 구현

--- pharmtrack/src/main/java/com/pharm/track/mapper/MedicineMapper.java
+++ pharmtrack/src/main/java/com/pharm/track/mapper/MedicineMapper.java
@@ -3,6 +3,9 @@ package com.pharm.track.mapper;
 import com.pharm.track.model.Medicine;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MedicineMapper {
     List<Medicine> findAll();
     Medicine findById(Long id);
